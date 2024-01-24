c7a2795c01824269c7a2f05d46c905d1382326d3
Clazariuk1 <Clazariuk@gmail.com>
2024-01-23 16:41:49 -0800

    movie routes made

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,1 +1,9 @@
 const Movie = require('../models/movie')
+// // POST /movies:
+// router.post('/', movieCtrl.create)
+// // GET /movies:
+// router.get('/', movieCtrl.index)
+// // GET /movies/:id
+// router.get('/:id', movieCtrl.show)
+// // POST /movies/moviewperformer
+// router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer) // MANY TO MANY
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,13 +1,17 @@
+//do the controller last because the routes inform what type of controllers are needed to be made.
+
 const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
-
-
-
-
-
-
+// POST /movies:
+router.post('/', movieCtrl.create)
+// GET /movies:
+router.get('/', movieCtrl.index)
+// GET /movies/:id
+router.get('/:id', movieCtrl.show)
+// POST /movies/moviewperformer
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer) // MANY TO MANY
 
 
 module.exports = router
